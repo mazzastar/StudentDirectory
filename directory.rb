4abb5c8c261a5f9723b83464b761e7f7d3ddfcc7
DLDB <dpbergquist@gmail.com>
2014-04-02 18:39:22 +0100

    refactoring to attempt to make 1 method do 1 thing

--- directory.rb
+++ directory.rb
@@ -1,3 +1,22 @@
+def append_fields(hash)
+	enterNewField = true
+	while enterNewField
+		print "Do you want to add another field to #{hash[:name]}'s information? (y/n)\n"
+		another_field = gets.chomp.to_s.upcase
+		if another_field == "Y"
+			print "What is the name of the field?\n"
+			new_field = gets.chomp
+			print "What is the value?\n"
+			new_val = gets.chomp
+			hash[new_field.to_sym]=new_val
+		else
+			enterNewField = false
+		end
+
+	end
+
+end
+
 def input_students
 	print "Please enter the names of the students\n"
 	print "To finish, just hit return twice\n"
@@ -9,22 +28,24 @@ def input_students
 	while !name.empty? do
 		# add the student hash to the array
 		students << {:name => name, :cohort => :november}
-		
+		append_fields(students[-1])
+		#Deal with extra fields
 
-		enterNewField= true
-		while enterNewField ==true# get another name from the user
-			print "Do you want to add another field to #{:name}'s information? (y/n)\n"
-			another_field = gets.chomp.to_s
-			if another_field.capitalize == "Y"
-				print "What is the name of the field?\n"
-				new_field = gets.chomp
-				print "What is the value?\n"
-				new_val = gets.chomp
-				students[-1][new_field.to_sym]=new_val
-			else
-				enterNewField = false
-			end
-		end
+		# enterNewField= true
+		
+		# while enterNewField ==true# get another name from the user
+		# 	print "Do you want to add another field to #{:name}'s information? (y/n)\n"
+		# 	another_field = gets.chomp.to_s
+		# 	if another_field.capitalize == "Y"
+		# 		print "What is the name of the field?\n"
+		# 		new_field = gets.chomp
+		# 		print "What is the value?\n"
+		# 		new_val = gets.chomp
+		# 		students[-1][new_field.to_sym]=new_val
+		# 	else
+		# 		enterNewField = false
+		# 	end
+		# end
 		print "Now we have #{students.length} students\nPlease enter the name of the next student\n"
 		name = gets.chomp
 	end
@@ -37,14 +58,22 @@ def print_header()
 	print "-----------\n"
 end
 
-def print_hash(list_entry)
-	list_entry.each do |key, value|
-		print "#{key} : #{value} \t"
+def print_hash(hash)
+	hash.each do |key, value|
+		print "#{key} : #{value}\t"
 	end
 	print "\n"
 end
 
-def print_rule(hash)
+def hash_to_string(hash)
+	string=""
+	hash.each do |key, value|
+		string << "#{key} : #{value}\t"
+	end
+	string
+end	
+
+def print_validation(hash)
 	hash[:name].split('')[0] == "A" && hash[:name].length<12
 end
 
@@ -54,13 +83,17 @@ def print_list(list)
 	i=0
 
 	while i<=max_index
-		print "#{i+1}. #{print_hash(list[i])} " if print_rule(list[i])
+		if print_validation(list[i])
+			print "#{i+1}. #{hash_to_string(list[i])}\n"
+		end 
+		# print "#{i+1}. #{print_hash(list[i])}\n" if print_rule(list[i])
 		i +=1
+
 	end
 end
 
 def print_footer(list)
-	print "Overall, we have #{list.count} students.\n\n"
+	print "\nOverall, we have #{list.count} students.\n\n"
 end
 
 students = input_students
