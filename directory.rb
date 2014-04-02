06f46e777e9c4eb1adfd413bf37c482e1a32e704
DLDB <dpbergquist@gmail.com>
2014-04-02 13:22:37 +0100

    made formatting a bit nicer

--- directory.rb
+++ directory.rb
@@ -1,6 +1,6 @@
 def input_students
-	puts "Please enter the names of the students"
-	puts "To finish, just hit return twice"
+	print "Please enter the names of the students\n"
+	print "To finish, just hit return twice\n"
 	# create an empty array
 	students = []
 	# get first name
@@ -9,7 +9,7 @@ def input_students
 	while !name.empty? do
 		# add the student hash to the array
 		students << {:name => name, :cohort => :november}
-		puts "Now we have #{students.length} students"
+		print "Now we have #{students.length} students\n"
 		# get another name from the user
 		name = gets.chomp
 	end
@@ -18,18 +18,18 @@ def input_students
 end
 
 def print_header()
-	puts "The students of my cohort at Makers Academy"
-	puts "-----------"
+	print "The students of my cohort at Makers Academy\n"
+	print "-----------\n"
 end
 
 def print_list(list)
 	list.each do |student_info|
-		puts "#{student_info[:name]} (#{student_info[:cohort]} cohort)"
+		print "#{student_info[:name]} (#{student_info[:cohort].to_s.capitalize} cohort)\n"
 	end
 end
 
 def print_footer(list)
-	puts "Overall, we have #{list.count} students."
+	print "Overall, we have #{list.count} students.\n\n"
 end
 
 students = input_students
